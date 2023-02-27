2e770ee92b44cbcfdf8125b817a27b75db377199
ShahZ-Khan <114197558+ShahZ-Khan@users.noreply.github.com>
2023-02-27 00:43:18 -0500

    files

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -31,7 +31,7 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     accessToken: API_KEY
 });
 // We create the dark view tile layer that will be an option for our map.
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -39,11 +39,11 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 
 // Accessing the airport GeoJSON URL
-let airportData ="https://raw.githubusercontent.com/ShahZ-Khan/Mapping_Earthquakes/Mapping_GeoJSON_Points/torontoRoutes.json"
+let airportData ="https://raw.githubusercontent.com/ShahZ-Khan/Mapping_Earthquakes/Mapping_GeoJSON_LineStrings/torontoNeighborhoods.json"
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-    center: [44.2, -80.0],
+    center: [43.7, -79.3],
     zoom: 2,
     layers: [streets]
 })
